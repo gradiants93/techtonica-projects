5308432135e4dfbbc2e3bdf17a51dbf56d28ccbf
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-01-27 11:29:05 -0800

    Clean up script

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -1,23 +1,10 @@
 // checkboxes next to ingredients
 
-
-
 //collect list of list elements
 document.getElementsByClassName
 let listOfLi = document.getElementsByClassName("ingredient-list")[0].children;
 // collect text of element
-/*function plugInText(input) {
-    let text = document.createTextNode();
-    text = input.innerHTML;
-    let label = document.createElement("label")
-    let checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
-    checkBox.setAttribute("name", `${input}checkbox`);
-    label.setAttribute("for", `${input}checkbox`);
-    label.innerText(text);
-    input.appendChild(checkBox);
-    input.appendChild(label);
-};*/
+
 for (element in listOfLi) {
     let text = listOfLi[element].innerHTML;
     let label = document.createElement("label")
@@ -30,12 +17,5 @@ for (element in listOfLi) {
     listOfLi[element].appendChild(label);
     
 }
-/*listOfChildren.forEach(element => plugInText(element))
-console.log(listOfLi, text)
-//create checkbox
-let checkBox = document.createElement("input")
-checkBox.setAttribute("type", "checkbox");
-listOfLi.appendChild(checkBox);
-console.log(checkBox)
-*/
+
 
