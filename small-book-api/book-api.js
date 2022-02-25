bbc24ee9b45b4dbcc5649d542bfbbc3b63c2bac1
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-02-24 21:52:07 -0800

    start to book api

---
+++ small-book-api/book-api.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+let books = [{}, {}, {}, {}, {}];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    // We will be coding here
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
