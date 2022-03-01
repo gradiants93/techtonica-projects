58e8845922bd5575cdc7bcdbb7a23e1e639092ab
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-01 14:08:38 -0800

    formatting

--- small-book-api/book-api.js
+++ small-book-api/book-api.js
@@ -1,32 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-let books = [{
-    isbn: '0553120441',
-    title: 'Dragonsong',
-    author: 'McCaffrey, Anne',
-    format: 'Paperback'
-}, {
-    isbn: '0679889175',
-    title: 'First Test',
-    author: 'Pierce, Tamora',
-    format: 'Paperback'
-}, {
-    isbn: '9780857660282',
-    title: 'Sixty-One Nails',
-    author: 'Shevdon, Mike',
-    format: 'Paperback'
-}, {
-    isbn: '0553096095',
-    title: 'The Diamond Age',
-    author: 'Stephenson, Neal',
-    format: 'Hardcover'
-}];
+let books = [
+  {
+    isbn: "0553120441",
+    title: "Dragonsong",
+    author: "McCaffrey, Anne",
+    format: "Paperback",
+  },
+  {
+    isbn: "0679889175",
+    title: "First Test",
+    author: "Pierce, Tamora",
+    format: "Paperback",
+  },
+  {
+    isbn: "9780857660282",
+    title: "Sixty-One Nails",
+    author: "Shevdon, Mike",
+    format: "Paperback",
+  },
+  {
+    isbn: "0553096095",
+    title: "The Diamond Age",
+    author: "Stephenson, Neal",
+    format: "Hardcover",
+  },
+];
 
 app.use(cors());
 
@@ -34,52 +39,54 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/book', (req, res) => {
-    res.sendFile(path.join(__dirname, '/new-book.html'));
+app.get("/book", (req, res) => {
+  res.sendFile(path.join(__dirname, "/new-book.html"));
 });
 
 //add book
-app.post('/book', (req, res) => {
-    const book = req.body;
-    //output to console
-    console.log(book);
-    books.push(book);
+app.post("/book", (req, res) => {
+  const book = req.body;
+  //output to console
+  console.log(book);
+  books.push(book);
 
-    res.send('Book was added to database');
+  res.send("Book was added to database");
 });
 
 //get books
-app.get('/books', (req, res) => {
-    res.json(books);
+app.get("/books", (req, res) => {
+  res.json(books);
 });
 
 //get specific book
-app.get('/book/:isbn', (req, res) => {
-    // Reading isbn from the URL
-    const isbn = req.params.isbn;
+app.get("/book/:isbn", (req, res) => {
+  // Reading isbn from the URL
+  const isbn = req.params.isbn;
 
-    for (let book of books) {
-        if (book.isbn === isbn) {
-            res.json(book);
-            return;
-        }
+  for (let book of books) {
+    if (book.isbn === isbn) {
+      res.json(book);
+      return;
     }
-    // Sending 404 when not found something is a good practice
-    res.status(404).send('Book not found');
+  }
+  // Sending 404 when not found something is a good practice
+  res.status(404).send("Book not found");
 });
 
-app.delete('/book/:isbn', (req, res) => {
-    // Reading isbn from the URL
-    const isbn = req.params.isbn;
+app.delete("/book/:isbn", (req, res) => {
+  // Reading isbn from the URL
+  const isbn = req.params.isbn;
 
-    // Remove item from the books array
-    books = books.filter(i => {
-        if (i.isbn !== isbn) {
-            return true;
-        }
-        return false;
-    });
+  // Remove item from the books array
+  books = books.filter((i) => {
+    if (i.isbn !== isbn) {
+      return true;
+    }
+    return false;
+  });
 
-    res.send('Book is deleted');
+  res.send("Book is deleted");
 });
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
+);
--- small-book-api/book-list.js
+++ small-book-api/book-list.js
@@ -1,26 +1,25 @@
-const setEditModal = (isbn) => {
-}
+const setEditModal = (isbn) => {};
 
 const deleteBook = (isbn) => {
-    const xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
 
-    xhttp.open("DELETE", `http://localhost:3000/book/${isbn}`, false);
-    xhttp.send();
+  xhttp.open("DELETE", `http://localhost:3000/book/${isbn}`, false);
+  xhttp.send();
 
-    // Reloading the page
-    location.reload();
-}
+  // Reloading the page
+  location.reload();
+};
 
 const loadBooks = () => {
-    const xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "http://localhost:3000/books", false);
-    xhttp.send();
+  xhttp.open("GET", "http://localhost:3000/books", false);
+  xhttp.send();
 
-    const books = JSON.parse(xhttp.responseText);
+  const books = JSON.parse(xhttp.responseText);
 
-    for (let book of books) {
-        const bookText = ` <div class="col-4">
+  for (let book of books) {
+    const bookText = ` <div class="col-4">
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">${book.title}</h5>
@@ -38,10 +37,11 @@ const loadBooks = () => {
                                 </button>
                             </div>
                         </div>
-                    </div>`
+                    </div>`;
 
-        document.getElementById('books').innerHTML = document.getElementById('books').innerHTML + bookText;
-    }
-}
+    document.getElementById("books").innerHTML =
+      document.getElementById("books").innerHTML + bookText;
+  }
+};
 
 loadBooks();
