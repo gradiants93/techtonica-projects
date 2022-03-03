c2f3f40d4bba47893fd402b9016d9fdd603be916
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-03 13:01:39 -0800

    placeholder forcast component

---
+++ weather-app/components/forcast.js
@@ -0,0 +1,6 @@
+import Express from "express";
+/*
+API call - api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid={API key}
+key - 8c911114f301aae0444d13de55d3fd59
+
+*/
