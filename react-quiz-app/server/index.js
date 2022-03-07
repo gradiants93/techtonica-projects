8360f2182c0af9191048bfa9adccbbade87f2c64
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-07 14:21:19 -0800

    added base route to check for functionality

--- react-quiz-app/server/index.js
+++ react-quiz-app/server/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3001;
 
+// Routes
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+// Make sure it's working terminal side
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
