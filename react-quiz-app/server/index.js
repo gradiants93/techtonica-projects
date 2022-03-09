b215ad28bb363ce5c0e0c44d6c762be0d7ac32a1
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-09 11:33:36 -0800

    WORKING | added nodemon so I don't have to keep refreshing server. Also added the quiz api call

--- react-quiz-app/server/index.js
+++ react-quiz-app/server/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3001;
+const fetch = require("node-fetch");
 
 // Routes
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Express!" });
+});
+app.get("/quiz", async (req, res) => {
+  let response = await fetch("https://opentdb.com/api.php?amount=10");
+  let quizQs = await response.json();
+  console.log(quizQs);
+  res.json(quizQs.results);
 });
 
 // Make sure it's working terminal side
