c911876c2c948548ddd6c8e241a1c003a8d03aa2
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-14 15:23:12 -0700

    WORKING added mock data to users compoent

--- Eventonica/client/src/components/Users.jsx
+++ Eventonica/client/src/components/Users.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Users() {
+  const marlin = { name: "Marlin", email: "marlin@gmail.com", id: "1" };
+  const nemo = { name: "Nemo", email: "nemo@gmail.com", id: "2" };
+  const dory = { name: "Dory", email: "dory@gmail.com", id: "3" };
+  const [users, setUsers] = useState([marlin, nemo, dory]);
   return (
     <section className="user-management">
       <h2>User Management</h2>
 
       <ul id="users-list">
         {/* display all existing Users here */}
-        <li>...</li>
+        {users.map((individual) => (
+          <li>
+            {individual.name} {individual.email}
+          </li>
+        ))}
       </ul>
 
       <div>
